a32956a28d04da9f9d898f08743856146d7e9dba
Uneeb159 <uneebk36@gmail.com>
2024-12-29 18:21:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function exploreMore() {
+  alert("Start your journey by exploring our Destinations page!");
+}
+
+const bookNowButton = document.getElementById('bookNow');
+
+
+bookNowButton.addEventListener('click', function() {
+  alert('Congratulations! Your booking is successfully done. Enjoy the journey' );
+});
+
+
